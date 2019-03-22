b1a2b0a972170402217721c3aebc6ef85c1f27ce
Frank Chen <frankchen850124@gmail.com>
2019-03-22 14:17:44 +0000

    update url to app.js

--- app.js
+++ app.js
@@ -18,9 +18,8 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index");
 
-mongoose.connect(process.env.DATABASEURL, { useNewUrlParser: true });   
-// mongoose.connect("mongodb+srv://frankchen850124:6ULvMg4KyuiiVfSd@cluster0-qwxo2.mongodb.net/test?retryWrites=true", { useNewUrlParser: true});
-
+var url = process.env.DATABASEURL || "mongodb://localhost:27017/yelp_camp_6";
+mongoose.connect(url, { useNewUrlParser: true });   
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
