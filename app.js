581cb89a4a0ab5628fef0214276dcb128da6b593
Frank Chen <frankchen850124@gmail.com>
2019-03-22 14:28:20 +0000

    update app.use bodyParser in app.js

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ var commentRoutes    = require("./routes/comments"),
 var url = process.env.DATABASEURL || "mongodb://localhost:27017/yelp_camp_6";
 mongoose.connect(url, { useNewUrlParser: true });   
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
