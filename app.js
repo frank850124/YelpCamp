646ce9abc82b9d9ad19361ccedfa7cf16f0a3af0
Frank Chen <frankchen850124@gmail.com>
2019-03-24 07:04:38 +0000

    fix reset v3

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var express     = require("express"),
     mongoose    = require("mongoose"),
     flash       = require("connect-flash"),
     passport    = require("passport"),
+    cookieParser = require("cookie-parser"),
     LocalStrategy = require("passport-local"),
     methodOverride = require("method-override"),
     Campground  = require("./models/campground"),
@@ -27,6 +28,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
+app.use(cookieParser('secret'));
 app.use(flash());
 app.locals.moment = require('moment');
 // seedDB();
